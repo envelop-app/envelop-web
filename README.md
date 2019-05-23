d2c5cdb57b5f6ffcfce79213a412eed4a006b82d
Joao Diogo Costa <jdscosta91@gmail.com>
2019-05-23 13:05:22 +0100

    Add download (/d/...) path

---
+++ README.md
@@ -0,0 +1,21 @@
+Recommended node version: `v10.15.3`
+
+Install dependencies:
+```
+yarn install
+```
+
+Start server:
+```
+DEBUG=blockstack-web:* npm start
+```
+
+Open in  browser:
+```
+http://localhost:3000
+```
+
+Test with sample file (previously uploaded):
+```
+http://localhost:3000/d/joaodiogocosta.id.blockstack/hello
+```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const GaiaFile = require('../lib/gaia_file');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Best Blockstack App Ever' });
+});
+
+router.get('/d/:username/:filename', function(req, res, next) {
+  new GaiaFile(req.app.get('appDomain'), req.params.username, req.params.filename)
+    .fetch()
+    .then((file) => res.render('download', { file: JSON.parse(file) }))
+    .catch(() => res.status(404).send('Not Found'));
 });
 
 module.exports = router;
