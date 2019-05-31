c12505c8c207b1408bf72717b4d731ea0c4e923b
Joao Diogo Costa <jdscosta91@gmail.com>
2019-05-31 16:44:56 +0100

    Refactor file fetching

--- routes/index.js
+++ routes/index.js
@@ -13,23 +13,9 @@ router.get('/d/:username/:filename', function(req, res, next) {
     username += '.id.blockstack';
   }
 
-  const gaiaFile = new GaiaFile(req.app.get('appDomain'), username, req.params.filename);
-
-  gaiaFile
-    .fetchInfo()
-    .then((infoJson) => {
-      const info = JSON.parse(infoJson);
-      
-      gaiaFile
-        .fetchFullUrl(info['url'])
-        .then((fullUrl) => {
-          res.render('download', {
-            name: info.url.split('/').pop(),
-            url: fullUrl,
-            size: info.size
-          });
-        });
-    })
+  new GaiaFile(req.app.get('appDomain'), username, req.params.filename)
+    .fetch()
+    .then(file => res.render('download', file))
     .catch(() => res.status(404).send('Not Found'));
 });
 
