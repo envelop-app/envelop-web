0580a1ccae5e726cd01da245e1c8f3c88f21ff4d
Sérgio Santos <me@sergiosantos.info>
2019-05-31 12:53:15 +0100

    Provide final URL for download

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/d/:username/:filename', function(req, res, next) {
-  new GaiaFile(req.app.get('appDomain'), req.params.username, req.params.filename)
-    .fetch()
-    .then((file) => res.render('download', { file: JSON.parse(file) }))
+  var username = req.params.username;
+  if (!username.includes('.')) {
+    username += '.id.blockstack';
+  }
+
+  const gaiaFile = new GaiaFile(req.app.get('appDomain'), username, req.params.filename);
+
+  gaiaFile
+    .fetchInfo()
+    .then((infoJson) => {
+      const info = JSON.parse(infoJson);
+      
+      gaiaFile
+        .fetchFullUrl(info['url'])
+        .then((fullUrl) => {
+          res.render('download', {
+            name: info.url.split('/').pop(),
+            url: fullUrl,
+            size: info.size
+          });
+        });
+    })
     .catch(() => res.status(404).send('Not Found'));
 });
 
