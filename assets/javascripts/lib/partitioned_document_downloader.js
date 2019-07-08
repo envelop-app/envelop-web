b938441fe78d6c145004aa6700cb94f8c52014c8
Joao Diogo Costa <jdscosta91@gmail.com>
2019-07-08 17:51:12 +0000

    Revoke object URL as per documentation to free up memory

--- assets/javascripts/lib/partitioned_document_downloader.js
+++ assets/javascripts/lib/partitioned_document_downloader.js
@@ -8,19 +8,34 @@ class PartitionedDocumentDownloader {
     this.limiter = new Bottleneck({ maxConcurrent: 3 });
   }
 
+  createBlob(partBuffers) {
+    const blobOptions = { name: this.gaiaDocument.getName(), type: this.gaiaDocument.getMimeType() };
+    return new Blob(partBuffers, blobOptions);
+  }
+
   async download() {
     const getPartBuffers = this.gaiaDocument.getPartUrls().map((partUrl) => {
       return this.limiter.schedule(() => this.downloadPart(partUrl));
     });
     const partBuffers = await Promise.all(getPartBuffers);
-    const blobOptions = { name: this.gaiaDocument.getName(), type: this.gaiaDocument.getMimeType() };
-    const blob = new Blob(partBuffers, blobOptions);
-    return URL.createObjectURL(blob);
+    const blob = this.createBlob(partBuffers);
+    const objectUrl = URL.createObjectURL(blob);
+
+    this.revokeLater(objectUrl)
+
+    return objectUrl;
   }
 
   async downloadPart(partUrl) {
     const options = { username: this.gaiaDocument._username, decrypt: false, verify: false };
     return await publicUserSession.getFile(partUrl, options);
+  }
+
+  revokeLater(objectUrl) {
+    window.addEventListener('focus', function handler() {
+      setTimeout(() => URL.revokeObjectURL(objectUrl), 1000);
+      window.removeEventListener('focus', handler);
+    });
   }
 }
 
