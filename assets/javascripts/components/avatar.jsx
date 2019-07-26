cd34a47d8816dad7259cd19d98e8558c8198c3da
Friedger Müffke <mail@friedger.de>
2019-07-26 09:49:24 +0200

    update app center url

--- assets/javascripts/components/avatar.jsx
+++ assets/javascripts/components/avatar.jsx
@@ -65,7 +65,7 @@ class AvatarComponent extends Component {
     } else if (item == 1) {
       window.open('mailto:feedback@envelop.app?subject=Envelop Feedback', '_blank');
     } else if (item == 2) {
-      window.open('https://app-center.openintents.org/appco/1453/comment', '_blank', 'noopener');
+      window.open('https://app-center.openintents.org/appco/1453/review', '_blank', 'noopener');
     } else if (item == 3) {
       privateUserSession.signUserOut();
       window.location = window.location.origin;
