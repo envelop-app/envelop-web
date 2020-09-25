cf87232891e6e6b025a342387a446a390c4378b7
FilipeA <filiperamosalves@gmail.com>
2020-09-25 11:49:56 +0100

    Updated authOptions scope

--- assets/javascripts/lib/blockstack_client.js
+++ assets/javascripts/lib/blockstack_client.js
@@ -1,17 +1,6 @@
 import {AppConfig, UserSession} from 'blockstack';
 import Constants from './constants';
 
-export const authOptions = {
-  redirectTo: '/',
-  finished: ({ }) => {
-    window.location = Constants.BLOCKSTACK_REDIRECT_URI;
-  },
-  appDetails: {
-    name: 'Envelop',
-    icon: 'https://envelop.app/images/manifest-icon.png',
-  },
-};
-
 function getUserSession(scopes) {
   const appConfig = new AppConfig(scopes, Constants.BLOCKSTACK_ORIGIN);
   return new UserSession({ appConfig: appConfig });
@@ -20,3 +9,14 @@ function getUserSession(scopes) {
 export const privateUserSession = getUserSession(['store_write', 'publish_data']);
 export const publicUserSession = getUserSession([]);
 
+export const authOptions = {
+  redirectTo: '/',
+  finished: ({ }) => {
+    window.location = Constants.BLOCKSTACK_REDIRECT_URI;
+  },
+  userSession: privateUserSession,
+  appDetails: {
+    name: 'Envelop',
+    icon: 'https://envelop.app/images/manifest-icon.png',
+  },
+};
